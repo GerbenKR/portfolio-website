16034c708a4c75f3a575c4162bfd40f178dc2a73
GerbenKr <gerbiebor@outlook.com>
2022-08-09 19:54:02 +0200

    Added redirect routes

--- next.config.js
+++ next.config.js
@@ -9,6 +9,56 @@ const nextConfig = {
                 destination: '/',
                 permanent: true,
             },
+            {
+                source: '/lingo',
+                destination: 'https://lingo.gerbenkr.nl/',
+                permanent: true,
+            },
+            {
+                source: '/omrekenen',
+                destination: 'https://lingo.gerbenkr.nl/',
+                permanent: true,
+            },
+            {
+                source: '/3fm-radioplayer',
+                destination: 'https://kevinschatbox.nl/chat',
+                permanent: true,
+            },
+            {
+                source: '/radioring',
+                destination: 'https://radioring-leaker.nl',
+                permanent: true,
+            },
+            {
+                source: '/whois',
+                destination: 'https://whois.gerbenkr.nl',
+                permanent: true,
+            },
+            {
+                source: '/magister',
+                destination: 'https://addons.mozilla.org/af/firefox/addon/magister-auto-week/',
+                permanent: true,
+            },
+            {
+                source: '/cps',
+                destination: 'https://projects.gerbenkr.nl/cps-counter',
+                permanent: true,
+            },
+            {
+                source: '/q-foute-uur',
+                destination: 'https://q-foute-uur.gerbenkr.nl',
+                permanent: true,
+            },
+            {
+                source: '/erik-jan-weerbericht',
+                destination: 'https://projects.gerbenkr.nl/erik-jan-weerbericht/',
+                permanent: true,
+            },
+            {
+                source: '/lingo',
+                destination: 'https://lingo.gerbenkr.nl/',
+                permanent: true,
+            },
         ];
     },
     images: {
